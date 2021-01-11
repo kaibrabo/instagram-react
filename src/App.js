d8f301a7ab834ea59cbbe3b2bbb492d9df0d9954
Kai <kai@Kais-MacBook-Pro.local>
2021-01-11 12:25:06 -0800

    Add Post component

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import './App.css';
 import Post from './Post';
 
 function App() {
-		return (
-			<div className="App">
-				<header className="app__header">
-					<img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="" className="app__headerImage" />
-				</header>
-				<section className="posts">
-					<Post />
-				</section>
-			</div>
-		);
+	return (
+		<div className="App">
+			<header className="app__header">
+				<img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="" className="app__headerImage" />
+			</header>
+			<section className="posts">
+				<Post username="kai_onthereal" caption="Wow its works" imageUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1000px-React-icon.svg.png" />
+				<Post username="wiserooster" caption="Wow its works" imageUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1000px-React-icon.svg.png" />
+				<Post username="_kchanguex" caption="Wow its works" imageUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1000px-React-icon.svg.png" />
+
+			</section>
+		</div>
+	);
 }
 
 export default App;
--- src/Post.js
+++ src/Post.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './Post.css';
+import Avatar from '@material-ui/core/Avatar';
 
-function Post() {
+function Post({username, caption, imageUrl}) {
     return (
         <div className='post'>
-            <h3> Username </h3>
-            <img className="post__img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1000px-React-icon.svg.png" alt=""/>
-            <h4 className="post__text"><strong>Username</strong> caption</h4>
+            <div className="post__header">
+                <Avatar className="post__avatar" src="" alt={username} />
+                <h3>{username}</h3>
+            </div>
+
+            <img className="post__img" src={imageUrl} alt="" />
+            
+            <h4 className="post__text"><strong>{username}</strong> {caption}</h4>
         </div>
     )
 }
